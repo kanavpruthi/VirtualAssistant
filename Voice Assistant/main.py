80cefa11611ee6b45a817da68fe677fcf6b96e50
Kanav Pruthi <kanav.pruthi@gmail.com>
2020-12-16 20:36:24 +0530

    updated opening applications

--- Voice Assistant/main.py
+++ Voice Assistant/main.py
@@ -1,3 +1,4 @@
+import webbrowser
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
@@ -5,6 +6,8 @@ import datetime
 import wikipedia as wiki
 import random
 import pyjokes as jk
+import subprocess as sb
+import os
 listener = sr.Recognizer()
 engine=pyttsx3.init()
 voices=engine.getProperty('voices')
@@ -15,6 +18,41 @@ def talk(text='Retinal scan confirmed, welcome Agent Johnson, I am Jarvis.'):
     engine.runAndWait()
 
 talk()
+def opensite(site):
+    webbrowser.open('http://www.'+site+'.com', new=2)
+def openapp(command):
+    if 'calculator' in command:
+            sb.Popen('C:\\Windows\\System32\\calc.exe')
+    elif 'notepad' in command:
+        talk('Opening Notepad')
+        os.system("Notepad")
+    elif 'wordpad' in command:
+        talk('Opening Wordpad')
+        sb.Popen('C:\\Windows\\System32\\write.exe')
+    elif 'google' in command or 'chrome' in command or 'browser' in command:
+        talk('Opening Google Chrome')
+        os.system('chrome')
+    elif ("vlcplayer" in command) or ("player" in command) or ("video player" in command) or ("5" in command):
+        talk('Opening VLC Media Player')
+        os.system("VLC")
+    elif ("illustrator" in command) or ("ai" in command) :
+        talk('Opening Adobe Illustrator')
+        os.system("illustrator")
+    elif ("photoshop" in command) or ("ps" in command):
+        talk('Opening Adobe Photoshop')
+        os.system("photoshop")
+    elif ("TELEGRAM" in command) or ("TG" in command) or ("10" in command):
+        talk('Opening Telegram')
+        os.system("telegram")
+    elif ("excel" in command) or ("msexcel" in command) or ("sheet" in command):
+        talk('Opening MS Excel')
+        os.system("excel")
+    elif ("slide" in command) or ("mspowerpoint" in command) or ("ppt" in command) or ("powerpnt" in command) :
+        talk('Opening Ms Powerpoint')
+        os.system("powerpnt")
+    elif ("word" in command) or ("msword" in command):
+        talk('Opening MS Word')
+        os.system("winword")
 
 def input_command():
     try:
@@ -65,12 +103,26 @@ def run_AI():
         talk(ls[int(random.random()*len(ls))])
     elif 'bored' in command or 'joke' in command or 'jokes' in command or 'boring' in command:
         talk(jk.get_joke())
+    elif 'open' in command:
+        openapp(command)
+        if 'youtube' in command:
+            opensite('youtube')
+        elif 'github' in command or 'git' in command or 'repository' in command:
+            opensite('github')
+        elif 'amazon'in command:
+            opensite('amazon')
+        elif 'flipkart' in command:
+            opensite('flipkart')
+        elif 'snapdeal' in command:
+            opensite('snapdeal')
+        elif 'gmail' in command:
+            opensite('gmail')
     elif 'stop' in command or 'exit' in command:
         talk('Shutting Jarvis down.')
         talk('Sorry to see that you are leaving.')
         return False
     else:
         talk('I am currently learning new things, please go easy on me')
-
+    return True
 while x:
     x=run_AI()
